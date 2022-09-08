1a29b2ace8d43c8e666d1c6fea263ae9319abcbe
lucankinnear <lucankinnear@gmail.com>
2022-09-08 08:02:03 -0700

    Fix betty error in task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - prints a phrase
+ * Return: 0 if executed successfully, no-zero otherwise
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
