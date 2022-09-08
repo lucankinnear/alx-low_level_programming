02c7518a6e6d68858a8811da8d13decdba3a3158
lucankinnear <lucankinnear@gmail.com>
2022-09-08 07:56:48 -0700

    Fix betty error in task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - prints a phrase
+ * Return: 0 if executed correctly, non-zero otherwise
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
