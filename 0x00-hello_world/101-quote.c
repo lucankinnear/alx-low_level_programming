e22ce4a89cdd4187af53540edd0eba65a2cafc5e
lucankinnear <lucankinnear@gmail.com>
2022-09-09 02:22:03 -0700

    Fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints a phrase
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Description: Prints a phrase,
+ * followed by a new line
+ *
+ * Return: Always (0) Success
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n', 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
