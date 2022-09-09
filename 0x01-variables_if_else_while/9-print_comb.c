2d6c89fc6552594fe603d78bc6d1e7b9e7462c9a
lucankinnear <lucankinnear@gmail.com>
2022-09-09 16:54:11 -0700

    Add task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int d;
+
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+
+return (0);
+
+}
