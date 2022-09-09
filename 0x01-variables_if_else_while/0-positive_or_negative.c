ff97903aefe3141d529e3391265fb0808cf4e3ab
lucankinnear <lucankinnear@gmail.com>
2022-09-09 15:02:42 -0700

    Fix task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlid>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Assigns a random number to the variable n
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+	printf("%d is positive\n", n);
+else if (n == 0)
+	printf("%d is zero\n", n);
+else
+	printf("%d is negative\n", n);
+return (0);
+}
