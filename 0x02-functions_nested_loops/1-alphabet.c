9bea30855ef55124d3f731506e3bdfe36774ca48
lucankinnear <lucankinnear@gmail.com>
2022-09-13 14:13:22 -0700

    Add task 1 and 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return 0
+ */
+
+void print_alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z'; j++)
+
+{
+	_putchar(j);
+
+}
+
+	_putchar('\n');
+
+}
+
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ *Return 0
+ */
+
+int main(void)
+
+{
+
+int j;
+int k = 0;
+
+while (k < 10)
+{
+	k++;
+
+	for (j = 'a'; j <= 'z'; j++)
+	
+	{
+		_putchar(j);
+
+	}
+
+_putchar('\n');
+
+}
+
+return 0;
+
+}
