64fff2441dafe6ae81dd4bc2d4b60ba6acdd6c15
lucankinnear <lucankinnear@gmail.com>
2022-09-13 18:54:11 -0700

    Add task 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return 0
+ */
+
+int add(int i, int k)
+
+{
+
+	return (i + k);
+
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return 0
+ */
+
+void print_to_98(int n)
+
+{
+
+if (n < 98)
+
+{
+
+	for (n = n; n < 98; n++)
+
+		printf("%d, ", n);
+
+		printf("%d\n", 98);
+
+}
+
+	else
+
+{
+
+	for (n = n; n > 98; n--)
+
+		printf("%d, ", n);
+
+		printf("%d\n", 98);
+
+}
+
+}
