ce7c5b76868115cb1142459971c248dde6babeb2
lucankinnear <lucankinnear@gmail.com>
2022-09-15 14:39:32 -0700

    Push 4 tasks

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+
+	{
+		return (1);
+
+	}
+
+	else
+
+	{
+
+		return (0);
+
+	}
+}
