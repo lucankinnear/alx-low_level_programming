79592d3eeb269ea675384cdfde31f23ae7987d59
lucankinnear <lucankinnear@gmail.com>
2022-09-22 04:41:01 -0700

    Add tasks 0 to 5

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - copies a string
+ * @dest: 
+ * @src: 
+ * @n: 
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest. src, n);
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - Changes lowercase to uppercase
+ * @str: the string
+ *
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+{
+	if (str[index] >= 'a' && str[index] <= 'z')
+		str[index] -= 32;
+	index++;
+}
+return (str);
+}
