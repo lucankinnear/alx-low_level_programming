e28fa2518f69d6e9492f933174f76dfe564f0063
lucankinnear <lucankinnear@gmail.com>
2022-09-26 05:44:10 -0700

    Add task 0

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
+char *_memset(char *, char, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+char *_strchar(char *, char);
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *, int);
+void set_string(char **, char *);
+
+#endif
