fba12807b8e050d6297d345ca4051b28a3771fe3
lucankinnear <lucankinnear@gmail.com>
2022-09-28 04:28:11 -0700

    Add tasks 0 to 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+include "main.h"
+
+int _sqrt(int, int);
+
+/**
+ * _sqrt_recursion - get the square root
+ * @n: the number
+ *
+ * Return: the answer
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+/**
+ * __sqrt - recursive square root
+ * @n: the number
+ * @i: iterator
+ *
+ * Return: number
+ */
+int _sqrt(int n, int i)
+{
+	int square = i * i;
+
+	if (square > n)
+
+		return (-1);
+
+	if (square == n)
+
+		return (i);
+
+	return (_sqrt(n, i + 1));
+}
