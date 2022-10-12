c01ef415832c24193b50caf60163a4fc64221116
lucankinnear <lucankinnear@gmail.com>
2022-10-13 00:56:28 +0200

    Add tasks

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * array_iterator - a function given as a parameter on each element of an array.
+ * @array: array
+ * @size: size of array
+ * @action: pointer to a function
+ * 
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+    int i;
+
+    if (array && size > 0 && action)
+    {
+        for (i = 0; i < size; i++)
+        {
+            action(array[i]);
+        }
+    }
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+/**
+ * op_add - returns sum of two numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: (@a + @b)
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns difference of two numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: (@a - @b)
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns product of two numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: (@a * @b)
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns quotient of two numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: (@a / @b)
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder from division of two numbers
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: (@a % @b)
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
